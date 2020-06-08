10f24dca7d7cfe22f17b303d525574a541671125
dani9oo <dani9oo.dev@gmail.com>
2020-06-08 00:03:24 -0500

    Added proper titles to navigation bar buttons

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ApplicationFormComponent } from '../application-form/application-form.component';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,7 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(ApplicationFormComponent, {
+      height: '49em',
+      width: '42em',
+      // height: '400px',
+      // width: '540px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 
   ngOnInit(): void {
   }
