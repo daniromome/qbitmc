995d17b7f490b4efc99d8727421a415b9ffa99aa
dani9oo <dani9oo.dev@gmail.com>
2020-06-08 01:23:19 -0500

    Added empty landing component

---
+++ src/app/landing/landing.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './landing.component.pug',
+  styleUrls: ['./landing.component.css']
+})
+export class LandingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
