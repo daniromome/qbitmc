bb512a4468240c51bfb8fd52a04a4108339fea3c
dani9oo <dani9oo.dev@gmail.com>
2020-06-08 00:03:37 -0500

    Initial commit for privacy policy

---
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './privacy-policy.component.pug',
+  styleUrls: ['./privacy-policy.component.css']
+})
+export class PrivacyPolicyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
