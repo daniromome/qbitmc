df3bdfb299800162ac4dd6a4187941610401d6d9
dani9oo <dani9oo.dev@gmail.com>
2020-06-08 00:02:52 -0500

    Initial about-us commit

---
+++ src/app/about-us/about-us.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './about-us.component.pug',
+  styleUrls: ['./about-us.component.css']
+})
+export class AboutUsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
