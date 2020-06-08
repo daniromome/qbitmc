7c074b0e87593022a02fe2cadb94232c1c0c6883
dani9oo <dani9oo.dev@gmail.com>
2020-06-08 01:24:35 -0500

    Initial commit for under development / 404 page

---
+++ src/app/wip/wip.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './wip.component.pug',
+  styleUrls: ['./wip.component.css']
+})
+export class WipComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
