3840fe53e243aa0aac605831221d92eae13c7c61
dani9oo <dani9oo.dev@gmail.com>
2020-06-08 01:19:44 -0500

    Added Routes for landing page aswell as 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { WipComponent } from './wip/wip.component';
+import { LandingComponent } from './landing/landing.component';
 
 
 const routes: Routes = [
+  { path: '', component: LandingComponent},
   { path: 'about-us', component: AboutUsComponent},
+  { path: '**', component: WipComponent}
 ];
 
 @NgModule({
