ec2f082e33bae925ed68e28a84b8892123279052
dani9oo <dani9oo.dev@gmail.com>
2020-06-08 00:03:50 -0500

    Initial commit for rules component

---
+++ src/app/rules/rules.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './rules.component.pug',
+  styleUrls: ['./rules.component.css']
+})
+export class RulesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
