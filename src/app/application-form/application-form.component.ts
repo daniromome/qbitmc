20a7020ea935e975c3ff1631232ac7a7d61b1e21
dani9oo <dani9oo.dev@gmail.com>
2020-06-11 01:28:38 -0500

    Restrict submissions until form completion

--- src/app/application-form/application-form.component.ts
+++ src/app/application-form/application-form.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { RulesComponent } from '../rules/rules.component';
 import { PrivacyPolicyComponent } from '../privacy-policy/privacy-policy.component';
+import { DataRequestService } from '../data-request.service';
 
 @Component({
   templateUrl: './application-form.component.pug',
@@ -10,10 +11,10 @@ import { PrivacyPolicyComponent } from '../privacy-policy/privacy-policy.compone
 })
 export class ApplicationFormComponent implements OnInit {
 
+  checked = false;
   applicationForm;
   ign;
   url;
-  Activities = new FormControl();
   activityList: string[] = [
     'Architect, I like building things and I consider myself really creative',
     'Redstone Mechanic, I enjoy doing redstone inventions for automation and fun',
@@ -22,6 +23,24 @@ export class ApplicationFormComponent implements OnInit {
     'Casual Crafter, I play every now and then to have some fun and entertain myself'
   ];
   safeUrlReg = '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?';
+  Forename  = new FormControl('', [
+    Validators.required
+  ]);
+  Discord  = new FormControl('', [
+    Validators.required
+  ]);
+  IGN = new FormControl('', [
+    Validators.required
+  ]);
+  Reasons = new FormControl('', [
+    Validators.required
+  ]);
+  Experiences = new FormControl('', [
+    Validators.required
+  ]);
+  Activities = new FormControl('', [
+    Validators.required
+  ]);
   linkFormControl = new FormControl('', [
     Validators.required,
     Validators.pattern(this.safeUrlReg),
@@ -29,13 +48,15 @@ export class ApplicationFormComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
+    private dataService: DataRequestService,
     public dialog: MatDialog
   ) {
     this.applicationForm = this.formBuilder.group({
-      Forename: '',
-      IGN: '',
-      Reasons: '',
-      Experiences: '',
+      Forename: this.Forename,
+      Discord: this.Discord,
+      IGN: this.IGN,
+      Reasons: this.Reasons,
+      Experiences: this.Experiences,
       Activities: this.Activities,
       PrevWork: this.linkFormControl,
       Agreement: ''
@@ -44,7 +65,7 @@ export class ApplicationFormComponent implements OnInit {
 
   openRules() {
     const dialogRef = this.dialog.open(RulesComponent, {
-      height: '49em',
+      height: '51em',
       width: '42em',
       // height: '400px',
       // width: '540px',
@@ -53,7 +74,7 @@ export class ApplicationFormComponent implements OnInit {
 
   openPrivacyPolicy() {
     const dialogRef = this.dialog.open(PrivacyPolicyComponent, {
-      height: '49em',
+      height: '51em',
       width: '42em',
       // height: '400px',
       // width: '540px',
@@ -61,7 +82,9 @@ export class ApplicationFormComponent implements OnInit {
   }
 
   onSubmit(applicationData) {
-    console.log('Your application has been submited', applicationData);
+    this.dataService.submitApplication(applicationData).subscribe((data: any[]) => {
+      console.log({ Message: 'Your application has been submited', Application: data});
+    });
   }
 
   onKey(value: string) {
