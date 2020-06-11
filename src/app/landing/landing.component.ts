d845d6be7917a8a7a8c5873b09cf058245778911
dani9oo <dani9oo.dev@gmail.com>
2020-06-11 01:28:59 -0500

    Initial commit for landing page

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { DataRequestService } from '../data-request.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ApplicationFormComponent } from '../application-form/application-form.component';
 
 @Component({
   templateUrl: './landing.component.pug',
@@ -6,9 +9,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  name: string;
+  instantInvite: string;
+  members: object[];
+  online: number;
+
+  ip: string;
+  status: number;
+  players: number;
+  maxPlayers: number;
+  playersList: [];
+
+  applications: object[];
+
+
+  constructor(
+    private dataService: DataRequestService,
+    public dialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
+    this.dataService.fetchDiscord().subscribe((data: any) => {
+      this.name = data.name;
+      this.instantInvite = data.instant_invite;
+      this.members = data.members;
+      this.online = data.presence_count;
+    });
+
+    this.dataService.fetchMinecraftStatistics().subscribe((data: any) => {
+      this.ip = data.domain;
+      this.status = data.status;
+      this.players = data.info.players;
+      this.maxPlayers = data.info.max_players;
+      this.playersList = data.info.players_list;
+    });
+
+    this.dataService.fetchApplications().subscribe((data: any) => {
+      this.applications = data.result;
+    });
+  }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(ApplicationFormComponent, {
+      height: '51em',
+      width: '42em',
+      // height: '400px',
+      // width: '540px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
 
 }
