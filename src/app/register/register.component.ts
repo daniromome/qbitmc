56d206d9f9a919c994ca5e6ca1b0d774c32b684a
dani9oo <dani9oo.dev@gmail.com>
2020-06-12 03:17:48 -0500

    Wrote functionality to user registration component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormBuilder, Validators } from '@angular/forms';
+import { DataRequestService } from '../data-request.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   templateUrl: './register.component.pug',
@@ -6,7 +9,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  email = new FormControl('', [
+    Validators.required,
+    Validators.email
+  ]);
+
+  password = new FormControl('', [
+    Validators.required,
+    Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]*'),
+    Validators.minLength(6)
+  ]);
+
+  confirmPassword = new FormControl('', [
+    Validators.required
+  ]);
+
+  ign = new FormControl('', [
+    Validators.required
+  ]);
+
+  registrationForm;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private dataRequestService: DataRequestService,
+    private snackBar: MatSnackBar
+  ) {
+    this.registrationForm = this.formBuilder.group({
+      IGN: this.ign,
+      Email: this.email,
+      Password: this.password
+    });
+  }
+
+  onKey() {
+    if (this.password !== this.confirmPassword) {
+      this.confirmPassword.setErrors({
+        pattern: 'Passwords do not match'
+      });
+    }
+  }
+
+  onSubmit(userData) {
+    let snackAlert: string;
+    this.dataRequestService.registerUser(userData).subscribe((data: any) => {
+      if (!data.success) { snackAlert = '✖️'; } else if (data.success) { snackAlert = '✔️'; }
+      this.snackBar.open(data.result, snackAlert, {
+        duration: 3000
+      });
+    });
+  }
 
   ngOnInit(): void {
   }
