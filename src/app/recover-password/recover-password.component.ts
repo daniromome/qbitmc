c5dcb096d9c43790c7697ef8d349ee2b69ced858
dani9oo <dani9oo.dev@gmail.com>
2020-06-11 19:14:01 -0500

    Initial commit

---
+++ src/app/recover-password/recover-password.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './recover-password.component.pug',
+  styleUrls: ['./recover-password.component.css']
+})
+export class RecoverPasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './register.component.pug',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
