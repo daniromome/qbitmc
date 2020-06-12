047f9a366a52b80eab8908be477d45a188ed5a43
dani9oo <dani9oo.dev@gmail.com>
2020-06-12 03:16:38 -0500

    Made password recovery component functional

--- src/app/recover-password/recover-password.component.ts
+++ src/app/recover-password/recover-password.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { DataRequestService } from '../data-request.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   templateUrl: './recover-password.component.pug',
@@ -6,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecoverPasswordComponent implements OnInit {
 
-  constructor() { }
+  Email = new FormControl('', [
+    Validators.email,
+    Validators.required
+  ]);
+
+  constructor(
+    private dataRequestService: DataRequestService,
+    private snackBar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(email) {
+    this.dataRequestService.recoverPassword(email).subscribe((data: any) => {
+      let snackAlert: string;
+      if (!data.success) { snackAlert = '✖️'; } else if (data.success) { snackAlert = '✔️'; }
+      this.snackBar.open(data.result, snackAlert, {
+        duration: 3000
+      });
+    });
   }
 
 }
