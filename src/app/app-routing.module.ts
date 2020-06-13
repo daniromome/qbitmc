d69d6948305c132f27b27e94e99305a576640fe5
dani9oo <dani9oo.dev@gmail.com>
2020-06-13 02:12:51 -0500

    Added route and AuthGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { WipComponent } from './wip/wip.component';
 import { LandingComponent } from './landing/landing.component';
+import { CommunityComponent } from './community/community.component';
+import { AuthGuard } from './auth.guard';
 
 
 const routes: Routes = [
-  { path: '', component: LandingComponent},
+  { path: '', component: LandingComponent },
   { path: 'about-us', component: AboutUsComponent},
+  { path: 'community', component: CommunityComponent, canActivate: [AuthGuard] },
   { path: '**', component: WipComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
