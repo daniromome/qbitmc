5b3d5d485a8bdabfa023aa9619a5cabeb35b0859
dani9oo <dani9oo.dev@gmail.com>
2020-06-13 02:13:58 -0500

    Revamped authentication to use rxjs

--- src/app/data-request.service.ts
+++ src/app/data-request.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
+import { User } from '../user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataRequestService {
 
+  private userSubject: BehaviorSubject<User>;
+  public user: Observable<User>;
+
   constructor(
-    private http: HttpClient
-  ) { }
+    private http: HttpClient,
+    private router: Router
+  ) {
+    this.userSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('user')));
+    this.user = this.userSubject.asObservable();
+  }
 
   submitApplication(application) {
     return this.http.post(`${environment.api}/application/submit`, application);
@@ -19,8 +30,27 @@ export class DataRequestService {
     return this.http.get(`${environment.api}/application/fetch`);
   }
 
+  public get userValue(): User {
+    return this.userSubject.value;
+  }
+
   logIn(userData) {
-    return this.http.post(`${environment.api}/user/logIn`, userData);
+    return this.http.post<any>(`${environment.api}/user/logIn`, userData)
+      .pipe(map(data => {
+        if (data.success) {
+          const user: User = new User(data.user);
+          localStorage.setItem('user', JSON.stringify(user));
+          this.userSubject.next(data);
+        } else {
+          this.userSubject.next(data);
+        }
+        return data;
+    }));
+  }
+
+  logOut() {
+    localStorage.removeItem('user');
+    this.router.navigate(['/']);
   }
 
   registerUser(userData){
