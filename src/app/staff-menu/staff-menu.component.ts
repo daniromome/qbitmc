32bf72de2f17f24f21368167f7b8c3d040b18c3e
dani9oo <dani9oo.dev@gmail.com>
2020-06-15 00:12:00 -0500

    Initial commit for staff menu component

---
+++ src/app/staff-menu/staff-menu.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './staff-menu.component.pug',
+  styleUrls: ['./staff-menu.component.css']
+})
+export class StaffMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
