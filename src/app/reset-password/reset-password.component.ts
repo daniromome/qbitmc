e441cce134f361dceb5fd3c2af6d0cebd39c4f32
dani9oo <dani9oo.dev@gmail.com>
2020-06-17 10:08:54 -0500

    initial commit

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators, FormBuilder } from '@angular/forms';
+import { DataRequestService } from '../data-request.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-reset-password',
@@ -7,9 +10,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetPasswordComponent implements OnInit {
 
-  constructor() { }
+  token;
+
+  password = new FormControl('', [
+    Validators.required,
+    Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]*'),
+    Validators.minLength(6)
+  ]);
+
+  confirm = new FormControl('', [
+    Validators.required
+  ]);
+
+  passwordForm;
+
+  constructor(
+    private dataRequestService: DataRequestService,
+    private readonly router: Router,
+    private readonly route: ActivatedRoute,
+    private formBuilder: FormBuilder
+  ) {
+    this.route.paramMap.subscribe(params => {
+      this.token = params.get('token');
+    });
+    this.passwordForm = this.formBuilder.group({
+      token: this.token,
+      Password: this.password
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  onKey() {
+    if (this.password !== this.confirm) {
+      this.confirm.setErrors({
+        pattern: 'Passwords do not match'
+      });
+    }
+  }
+
+  onSubmit(userData) {
+    this.dataRequestService.resetPassword(userData).subscribe((data: any) => {
+      if (data.success) {
+        this.router.navigateByUrl('/', {
+          queryParams: { passwordReset : true }
+        });
+      } else { this.router.navigate(['/'], {
+        queryParams: { passwordReset : false, result: data.result }
+      }); }
+    });
   }
 
 }
