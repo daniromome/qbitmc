839d3262d26247f7766bb8739100a8cd0be3f2c9
dani9oo <dani9oo.dev@gmail.com>
2020-06-17 23:23:14 -0500

    Announce component initial commit

---
+++ src/app/announce/announce.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './announce.component.pug',
+  styleUrls: ['./announce.component.css']
+})
+export class AnnounceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
